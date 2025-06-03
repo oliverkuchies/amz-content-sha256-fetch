395f3220c144a6f479141b6a949d0a178d954eba
Oliver Kucharzewski <oliver@olidev.com.au>
2025-06-03 12:05:29 +1000

    Bump version to 1.0.6; refactor hashBody and mutateHeaders to be async and support FormData

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -27,11 +27,11 @@ beforeEach(() => {
 });
 
 describe('hashBody', () => {
-    it('should hash the body correctly', () => {
+    it('should hash the body correctly', async () => {
         const body = { key: 'value' }
         const expectedHash = 'e43abcf3375244839c012f9633f95862d232a95b00d5bc7348b3098b9fed7f32'
                 
-        const result = hashBody(body)
+        const result = await hashBody(body)
         
         expect(result).toBe(expectedHash)
     })
@@ -76,18 +76,100 @@ describe('fetchWithSha256Headers', () => {
         const response = await fetchSha256(url, options)
         expect(response.status).toBe(200)
     })
-})
+
+    it('should add hashed body to the headers for form data', async () => {
+        const url = 'https://example.com'
+        const body = new URLSearchParams({ key: 'bobs-bananas', mango: 'fruit' })
+    
+        const options = {
+            method: 'POST',
+            url: url,
+            body: body.toString(),
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded'
+            }
+        }
+        
+        const response = await fetchSha256(url, options)
+        expect(response.status).toBe(200)
+    });
+
+    it('should add hashed body to the headers for form data', async () => {
+        const url = 'https://example.com'
+        const body = new URLSearchParams({ key: 'bobs-bananas', mango: 'fruit' })
+
+        const options = {
+            method: 'POST',
+            url: url,
+            body: body.toString(),
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded'
+            }
+        }
+
+        const response = await fetchSha256(url, options)
+        expect(response.status).toBe(200)
+    });
+
+    it('should add hashed body to the headers for multipart/form-data', async () => {
+        const url = 'https://example.com'
+        const body = new FormData()
+        body.append('file', new Blob(['file content'], { type: 'text/plain' }), 'file.txt')
+        body.append('key', 'bobs-bananas')
+        body.append('mango', 'fruit')
+
+        const options = {
+            method: 'POST',
+            url: url,
+            body: body,
+            headers: {
+                'Content-Type': 'multipart/form-data'
+            }
+        }
+
+        const response = await fetchSha256(url, options)
+        expect(response.status).toBe(200)
+    });
+});
 
 describe('mutateHeaders', () => {
-    it('should add the hashed body to the headers', () => {
+    it('should add the hashed body to the headers', async () => {
         const headers = {
             'Content-Type': 'application/json'
         }
         const body = { key: 'bobs-bananas', 'mango': 'fruit' }
         const expectedHash = '8a573ca735c107f6545e687ef82913a9f3b50310b7f4c6b013fc4bacf63f11d4'
         
-        const result = mutateHeaders(headers, body)
+        const result = await mutateHeaders(headers, body)
         
         expect(result[AWS_CONTENT_SHA256_HEADER]).toBe(expectedHash)
     })
+
+    it('should support form data', async () => {
+        const headers = {
+            'Content-Type': 'application/x-www-form-urlencoded'
+        }
+        const body = new URLSearchParams({ key: 'bobs-bananas', mango: 'fruit' })
+        const expectedHash = '44136fa355b3678a1146ad16f7e8649e94fb4fc21fe77e8310c060f61caaff8a'
+        
+        const result = await mutateHeaders(headers, body)
+        
+        expect(result[AWS_CONTENT_SHA256_HEADER]).toBe(expectedHash)
+    });
+
+    it('should support file via form data', async () => {
+        const headers = {
+            'Content-Type': 'multipart/form-data'
+        }
+        const body = new FormData()
+        body.append('file', new Blob(['file content'], { type: 'text/plain' }), 'file.txt')
+        body.append('key', 'bobs-bananas')
+        body.append('mango', 'fruit')
+
+        const expectedHash = 'e0ac3601005dfa1864f5392aabaf7d898b1b5bab854f1acb4491bcd806b76b0c'
+        
+        const result = await mutateHeaders(headers, body)
+        
+        expect(result[AWS_CONTENT_SHA256_HEADER]).toBe(expectedHash)
+    });
 });
--- src/index.ts
+++ src/index.ts
@@ -2,23 +2,37 @@ import {
     sha256
 } from 'js-sha256'
 
+
 export const AWS_CONTENT_SHA256_HEADER = 'x-amz-content-sha256'
 
-export function hashBody(body : unknown) {
+export async function hashBody(body : unknown) {
     if (typeof body === 'string') {
         return sha256(body).replace(/\n/g, '')
     }
+
+    if (body instanceof FormData && body.has('file') && body.get('file') instanceof File) {
+        return transformFileToSHA256(body.get('file') as File)
+    }
+    
     
     const bodyAsString = JSON.stringify(body).replace(/\/\n/g, '\n');
-    return sha256(bodyAsString)
+    return sha256(bodyAsString);
 }
 
-export function mutateHeaders(headers: HeadersInit, body: unknown) {
+export async function mutateHeaders(headers: HeadersInit, body: unknown) {
     return {
         ...headers,
-        [AWS_CONTENT_SHA256_HEADER]: hashBody(body)
+        [AWS_CONTENT_SHA256_HEADER]: await hashBody(body)
     }
 }
+
+async function transformFileToSHA256(file : File): Promise<string> {
+    const arrayBuffer = await file.arrayBuffer();
+    const hashBuffer = await crypto.subtle.digest('SHA-256', arrayBuffer);
+    const hashArray = Array.from(new Uint8Array(hashBuffer)); 
+    const hashHex = hashArray.map(b => b.toString(16).padStart(2, '0')).join('');
+    return hashHex;
+  }
 
 export async function fetchSha256(url: string, options: RequestInit = {}) {
     if (options.body === undefined) {
@@ -27,7 +41,7 @@ export async function fetchSha256(url: string, options: RequestInit = {}) {
 
     const optionsWithHashedHeader = {
         ...options,
-        headers: mutateHeaders(options.headers ?? {}, options.body)
+        headers: await mutateHeaders(options.headers ?? {}, options.body)
     }
 
     return fetch(url, optionsWithHashedHeader)
